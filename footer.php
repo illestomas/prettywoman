bb162cb672d0a81caf2d8eb2e9454a5a7e382dfd
Illes Tamas <illestomi@azet.sk>
2021-01-31 19:29:12 +0100

    Footer styling.

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
 <div class="footer">
+  <div class="container footer-logo-container">
+    <img src="<?php echo get_template_directory_uri() . '/src/pwbt_white.svg'; ?>" />
+  </div>
   <div class="container">
     <?php wp_nav_menu(); ?>
+  </div>
+  <div class="container">
+    <p class="copyright">© 
+    <?php
+      $date = getdate();
+      echo $date['year'];
+    ?>&nbsp;Minden jog fenntartva</p>
+
   </div>
 </div>
 
