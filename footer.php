ce8bdd87ec7027060d31aa8030206c62f4d6491b
Illes Tamas <illestomi@azet.sk>
2021-01-31 19:09:29 +0100

    Footer design.

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
-<!-- <div class="footer">
-  <img src="<?php echo get_template_directory_uri() . '/src/pwbt_white.svg'; ?>" />
-</div> -->
+<div class="footer">
+  <div class="container">
+    <?php wp_nav_menu(); ?>
+  </div>
+</div>
 
 <?php wp_footer(); ?>
 <script src="<?php echo get_template_directory_uri() . '/build/bundle.js' ?>"></script>
